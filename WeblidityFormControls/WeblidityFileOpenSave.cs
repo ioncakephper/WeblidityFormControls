ddb6f3539d5d2fe64ce6a398ad4734b3c65a5110
Ion Gireada <shytiger@yahoo.com>
2021-05-25 13:41:28 +0300

    Add files via upload

---
+++ WeblidityFormControls/WeblidityFileOpenSave.cs
@@ -0,0 +1,316 @@
+﻿//=============================================
+// File Name: WeblidityFileOpenSave.cs
+// Code Formatted on: 5/21/2021 7:35:46 PM
+//
+// Author: Ion Gireada 
+//=============================================
+
+namespace WeblidityFormControls
+{
+    using System;
+    using System.ComponentModel;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Defines the <see cref="WeblidityFileOpenSave" />.
+    /// </summary>
+    public partial class WeblidityFileOpenSave : Component
+    {
+        /// <summary>
+        /// Defines the e.
+        /// </summary>
+        private FileOpenSaveEventArgs e;
+
+        /// <summary>
+        /// Defines the FileSave.
+        /// </summary>
+        /// 
+        [Description("Occurs when content needs to be saved")]
+        public event EventHandler<FileOpenSaveEventArgs> FileSave;
+
+        /// <summary>
+        /// Defines the BeforeFileSave.
+        /// </summary>
+        /// 
+        [Description("Occurs before saving the content")]
+        public event EventHandler<FileOpenSaveEventArgs> BeforeFileSave;
+
+        /// <summary>
+        /// Defines the AfterFileSave.
+        /// </summary>
+        /// 
+        [Description("Occurs after saving the content")]
+        public event EventHandler<FileOpenSaveEventArgs> AfterFileSave;
+
+        /// <summary>
+        /// Defines the FileOpen.
+        /// </summary>
+        /// 
+        [Description("Occurs when content needs to be open")]
+        public event EventHandler<FileOpenSaveEventArgs> FileOpen;
+
+        /// <summary>
+        /// Defines the BeforeFileOpen.
+        /// </summary>
+        /// 
+        [Description("Occurs before opening the content")]
+        public event EventHandler<FileOpenSaveEventArgs> BeforeFileOpen;
+
+        /// <summary>
+        /// Defines the AfterFileOpen.
+        /// </summary>
+        /// 
+        [Description("Occurs after opening the content")]
+        public event EventHandler<FileOpenSaveEventArgs> AfterFileOpen;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeblidityFileOpenSave"/> class.
+        /// </summary>
+        public WeblidityFileOpenSave()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeblidityFileOpenSave"/> class.
+        /// </summary>
+        /// <param name="container">The container<see cref="IContainer"/>.</param>
+        public WeblidityFileOpenSave(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Saves content in a specified filename.
+        /// </summary>
+        /// <param name="fileName">The fileName<see cref="string"/>.</param>
+        /// <returns>The <see cref="FileOpenSaveResult"/>.</returns>
+        public FileOpenSaveResult SaveAs(string fileName)
+        {
+            FileName = fileName;
+            return SaveAs();
+        }
+
+        /// <summary>
+        /// Gets or sets the FileName.
+        /// </summary>
+        /// 
+        [DefaultValue("")]
+        [Description("The filename where content is saved to or opened from")]
+        public string FileName { get; set; } = "";
+
+        /// <summary>
+        /// Gets or sets the SaveFileDialog.
+        /// </summary>
+        /// 
+        [Description("The dialog to use when choosing a filename to save into")]
+        public SaveFileDialog SaveFileDialog { get; set; } = new SaveFileDialog();
+
+        /// <summary>
+        /// Gets or sets the OpenFileDialog.
+        /// </summary>
+        /// 
+        [Description("The dialog to use when choosing a filename to open")]
+        public OpenFileDialog OpenFileDialog { get; set; } = new OpenFileDialog();
+
+        /// <summary>
+        /// Save content in current filename or ask for a filename.
+        /// </summary>
+        /// <returns>The <see cref="FileOpenSaveResult"/>.</returns>
+        /// 
+        public FileOpenSaveResult Save()
+        {
+            if (!FileNameExists())
+            {
+                return SaveAs();
+            }
+            OnFileSave(FileName);
+            return e.Errors ? FileOpenSaveResult.Failure : FileOpenSaveResult.Success;
+        }
+
+        /// <summary>
+        /// Invokes the FileSave event code. The code executes unless cancelled in BeforeFileSave event.
+        /// </summary>
+        /// <param name="fileName">The fileName<see cref="string"/>.</param>
+        protected void OnFileSave(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException("message", nameof(fileName));
+            }
+            e = new FileOpenSaveEventArgs()
+            {
+                FileName = fileName
+            };
+
+            if (FileSave != null)
+            {
+
+                if (BeforeFileSave != null)
+                {
+                    BeforeFileSave(this, e);
+                }
+
+                if (!e.Cancel)
+                {
+                    FileSave(this, e);
+                }
+
+                if (AfterFileSave != null)
+                {
+                    AfterFileSave(this, e);
+                }
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// Open file content and store it in LoadedObject property of the FileOpenSaveEventArgs object.
+        /// </summary>
+        /// <returns>The <see cref="FileOpenSaveResult"/>.</returns>
+        public FileOpenSaveResult Open()
+        {
+            var d = new OpenFileDialog();
+            if (d.ShowDialog() == DialogResult.OK)
+            {
+                if (Save() == FileOpenSaveResult.Success)
+                {
+                    FileName = d.FileName;
+                    if (FileNameExists(true))
+                    {
+                        OnFileOpen(FileName);
+
+                        return e.Errors ? FileOpenSaveResult.Failure : FileOpenSaveResult.Success;
+                    }
+                }
+            }
+            return FileOpenSaveResult.Cancel;
+        }
+
+        /// <summary>
+        /// The OnFileOpen.
+        /// </summary>
+        /// <param name="fileName">The fileName<see cref="string"/>.</param>
+        protected void OnFileOpen(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException("message", nameof(fileName));
+            }
+
+            e = new FileOpenSaveEventArgs() { FileName = fileName };
+            if (FileOpen != null)
+            {
+                if (BeforeFileOpen != null)
+                {
+                    BeforeFileOpen(this, e);
+                }
+                if (!e.Cancel)
+                {
+                    FileOpen(this, e);
+                }
+                if (AfterFileOpen != null)
+                {
+                    AfterFileOpen(this, e);
+                }
+            }
+        }
+
+        /// <summary>
+        /// The FileNameExists.
+        /// </summary>
+        /// <returns>The <see cref="bool"/>.</returns>
+        private bool FileNameExists()
+        {
+            return FileNameExists(false);
+        }
+
+        /// <summary>
+        /// The FileNameExists.
+        /// </summary>
+        /// <param name="isForOpen">The isForOpen<see cref="bool"/>.</param>
+        /// <returns>The <see cref="bool"/>.</returns>
+        private bool FileNameExists(bool isForOpen)
+        {
+            bool r = !string.IsNullOrWhiteSpace(FileName);
+            return isForOpen ? r && System.IO.File.Exists(FileName) : r;
+        }
+
+        /// <summary>
+        /// The SaveAs.
+        /// </summary>
+        /// <returns>The <see cref="FileOpenSaveResult"/>.</returns>
+        public FileOpenSaveResult SaveAs()
+        {
+            SaveFileDialog d = new SaveFileDialog();
+            d.FileName = FileName;
+            if (d.ShowDialog() == DialogResult.OK)
+            {
+                return Save(d.FileName);
+            }
+            return FileOpenSaveResult.Cancel;
+        }
+
+        /// <summary>
+        /// The Save.
+        /// </summary>
+        /// <param name="fileName">The fileName<see cref="string"/>.</param>
+        /// <returns>The <see cref="FileOpenSaveResult"/>.</returns>
+        private FileOpenSaveResult Save(string fileName)
+        {
+            FileName = fileName;
+            return Save();
+        }
+    }
+
+    /// <summary>
+    /// Defines the <see cref="FileOpenSaveEventArgs" />.
+    /// </summary>
+    public class FileOpenSaveEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets or sets the FileName.
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether Cancel.
+        /// </summary>
+        public bool Cancel { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether Errors.
+        /// </summary>
+        public bool Errors { get; set; }
+
+        /// <summary>
+        /// Gets or sets the LoadedObject.
+        /// </summary>
+        public object LoadedObject { get; set; }
+    }
+
+    /// <summary>
+    /// Defines the FileOpenSaveResult.
+    /// </summary>
+    public enum FileOpenSaveResult
+    {
+        /// <summary>
+        /// Defines the Success.
+        /// </summary>
+        Success,
+
+        /// <summary>
+        /// Defines the Failure.
+        /// </summary>
+        Failure,
+
+        /// <summary>
+        /// Defines the Cancel.
+        /// </summary>
+        Cancel
+    }
+}
---
+++ WeblidityFormControls/WeblidityFormCloser.cs
@@ -0,0 +1,189 @@
+﻿//=============================================
+// File Name: WeblidityFormCloser.cs
+// Code Formatted on: 5/21/2021 10:28:42 AM
+//
+// Author: Ion Gireada 
+//=============================================
+
+namespace WeblidityFormControls
+{
+    using System;
+    using System.ComponentModel;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Defines the <see cref="WeblidityFormCloser" />.
+    /// </summary>
+    public partial class WeblidityFormCloser : Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeblidityFormCloser"/> class.
+        /// </summary>
+        public WeblidityFormCloser()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeblidityFormCloser"/> class.
+        /// </summary>
+        /// <param name="container">The container<see cref="IContainer"/>.</param>
+        public WeblidityFormCloser(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets the ConfirmationDialogResult.
+        /// </summary>
+        [Browsable(false)]
+        public DialogResult ConfirmationDialogResult { get; set; }
+
+        /// <summary>
+        /// Gets or sets the KeepFormOpenResult.
+        /// </summary>
+        /// 
+        [DefaultValue(DialogResult.Cancel)]
+        [Description("Specifies which button keeps the form opened.")]
+        public DialogResult KeepFormOpenResult { get; set; } = DialogResult.Cancel;
+
+        /// <summary>
+        /// Gets or sets the Caption.
+        /// </summary>
+        [DefaultValue("Confirm form closing")]
+        [Description("The title of confirmation dialog")]
+        public string Caption { get; set; } = "Confirm form closing";
+
+        /// <summary>
+        /// Gets or sets the ButtonSet.
+        /// </summary>
+        /// 
+        [DefaultValue(ButtonSetOptions.OKCancel)]
+        [Description("Determines which set of buttons appear in confirmation dialog")]
+        public ButtonSetOptions ButtonSet { get; set; } = ButtonSetOptions.OKCancel;
+
+        /// <summary>
+        /// Gets or sets the Icon.
+        /// </summary>
+        /// 
+        [DefaultValue(MessageBoxIcon.Question)]
+        [Description("The icon to show in confirmation dialog")]
+        public MessageBoxIcon Icon { get; set; } = MessageBoxIcon.Question;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether IsDirty.
+        /// </summary>
+        [DefaultValue(false)]
+        [Description("Indicates whether form content has changed")]
+        public bool IsDirty { get; set; }
+
+        /// <summary>
+        /// Gets or sets the TextLine1.
+        /// </summary>
+        /// 
+        [DefaultValue("Dialog content changed.")]
+        [Description("The first line of text in confirmation dialog for OKCancel button set")]
+        public string TextLine1 { get; set; } = "Dialog content changed.";
+
+        /// <summary>
+        /// Gets or sets the TextLine2.
+        /// </summary>
+        /// 
+        [DefaultValue("If you close the dialog, dialog content changes will be lost. If you want to close the dialog and lose the changes, click OK. To keep the dialog open, click Cancel.")]
+        [Description("The second line of text in confirmation dialog for OKCancel button set")]
+        public string TextLine2 { get; set; } = "If you close the dialog, dialog content changes will be lost. If you want to close the dialog and lose the changes, click OK. To keep the dialog open, click Cancel.";
+
+        /// <summary>
+        /// Gets or sets the TextLine3.
+        /// </summary>
+        /// 
+        [DefaultValue("Are you sure you want to close the form?")]
+        [Description("The third line of text in confirmation dialog for OKCancel button set")]
+        public string TextLine3 { get; set; } = "Are you sure you want to close the form?";
+
+        /// <summary>
+        /// Gets or sets the TextLine4.
+        /// </summary>
+        /// 
+        [DefaultValue("Form content changed.")]
+        [Description("The first line of text in confirmation dialog for YesNoCancel button set")]
+        public string TextLine4 { get; set; } = "Form content changed.";
+
+        /// <summary>
+        /// Gets or sets the TextLine5.
+        /// </summary>
+        /// 
+        [DefaultValue("If you want to close the form and save its content, click Yes. To close the form, but lose the changes, click No. To keep the form open, click Cancel.")]
+        [Description("The second line of text in confirmation dialog for YesNoCancel button set")]
+        public string TextLine5 { get; set; } = "If you want to close the form and save its content, click Yes. To close the form, but lose the changes, click No. To keep the form open, click Cancel.";
+
+        /// <summary>
+        /// Gets or sets the TextLine6.
+        /// </summary>
+        /// 
+        [DefaultValue("Do you want to close the form and save its content?")]
+        [Description("The third line of text in confirmation dialog for YesNoCancel button set")]
+        public string TextLine6 { get; set; } = "Do you want to close the form and save its content?";
+
+        /// <summary>
+        /// Asks user to confirm closing the form by showing the closing dialog.
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/>.</param>
+        /// <param name="e">The e<see cref="FormClosingEventArgs"/>.</param>
+        public void ConfirmFormClosing(object sender, FormClosingEventArgs e)
+        {
+            Form s = (Form)sender;
+            bool shouldConfirmClosing = (s.DialogResult == DialogResult.Cancel) || (s.DialogResult == DialogResult.None);
+
+            if (IsDirty && shouldConfirmClosing)
+            {
+                MessageBoxButtons buttons = (ButtonSet == ButtonSetOptions.OKCancel) ? MessageBoxButtons.OKCancel : MessageBoxButtons.YesNoCancel;
+                var defaultButton = (ButtonSet == ButtonSetOptions.OKCancel) ? MessageBoxDefaultButton.Button2 : MessageBoxDefaultButton.Button3;
+
+                ConfirmationDialogResult = MessageBox.Show(GetTextLines(), Caption, buttons, Icon, defaultButton);
+                e.Cancel = (ConfirmationDialogResult == KeepFormOpenResult);
+            }
+        }
+
+        /// <summary>
+        /// The GetTextLines.
+        /// </summary>
+        /// <returns>The <see cref="string"/>.</returns>
+        private string GetTextLines()
+        {
+            string[] lines = new string[3];
+            if (ButtonSet == ButtonSetOptions.OKCancel)
+            {
+                lines[0] = TextLine1;
+                lines[1] = TextLine2;
+                lines[2] = TextLine3;
+            }
+            else
+            {
+                lines[0] = TextLine4;
+                lines[1] = TextLine5;
+                lines[2] = TextLine6;
+            }
+
+            return string.Format(@"{1}{0}{0}{2}{0}{0}{3}", Environment.NewLine, lines[0], lines[1], lines[2]);
+        }
+    }
+
+    /// <summary>
+    /// Defines the ButtonSetOptions.
+    /// </summary>
+    public enum ButtonSetOptions
+    {
+        /// <summary>
+        /// Defines the OKCancel.
+        /// </summary>
+        OKCancel = MessageBoxButtons.OKCancel,
+
+        /// <summary>
+        /// Defines the YesNoCancel.
+        /// </summary>
+        YesNoCancel = MessageBoxButtons.YesNoCancel
+    }
+}
